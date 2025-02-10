8ed5023a4be9db6d9b056981ae3051c530f97fe5
mdmhriday <mdmhriday@gmail.com>
2025-02-10 17:28:27 +0100

    add transforme class

--- models/components/transformer/transformer.py
+++ models/components/transformer/transformer.py
@@ -32,3 +32,14 @@ class TransformerBlock(nn.Module):
         x = x + self.ffb(x)
         return x
 
+class Transformer(nn.Module):
+    def __init__(Self, num_layers, dim, num_heads=8, hidden_dim=None, dropout=0.1):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            TransformerBlock(dim, num_heads, hidden_dim, dropout) for _ in range(num_layers)
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(input)
+            return x
