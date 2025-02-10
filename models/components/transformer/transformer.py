1cceb6391d898cfda191131a82e176abd271c9bf
mdmhriday <mdmhriday@gmail.com>
2025-02-10 17:23:30 +0100

    add transformer block class

---
+++ models/components/transformer/transformer.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from ..attention.attention import MultiHeadAttention
+
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, input):
+        return self.layers(input)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, num_heads=8, hidden_dim=None, dropout=0.1):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(dim)
+        self.attn = MultiHeadAttention(dim, num_heads, dropout)
+        self.norm2 = nn.LayerNorm(dim)
+        self.ffb = FeedForwardBlock(dim, hidden_dim or dim*4, dropout=dropout)
+
+    def forward(self, input):
+        x = self.norm1(input)
+        x = x + self.attn(x)
+        x = self.norm2(x)
+        x = x + self.ffb(x)
+        return x
+
